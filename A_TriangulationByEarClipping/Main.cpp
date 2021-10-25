a40af1c559b10ee334cc0c810ec658bb2417299a
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2021-10-25 23:15:08 +0800

    remove x64

--- A_TriangulationByEarClipping/Main.cpp
+++ A_TriangulationByEarClipping/Main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
 
-int main(int argc, char* argv[])
+#pragma comment(lib,"user32")
+#pragma comment(lib,"gdi32")
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+HWND WINAPI GetConsoleWindow();
+int main(int argc,char *argv[])
 {
-    std::cout << "Hello Triangulation By Ear Clipping!" << std::endl;
+    HWND hwnd;
+    HDC hdc;
+    HPEN hpen;
+    hwnd = GetConsoleWindow();
+    hdc = GetDC(hwnd);
+    system("color F0");
+    system("cls");
+    TextOut(hdc,50,100,nullptr,12);
+    hpen=CreatePen(PS_SOLID,1,RGB(255,0,0));
+    SelectObject(hdc,hpen);
+    MoveToEx(hdc,20,20,NULL);
+    LineTo(hdc,200,300);
+    DeleteObject(hpen);
+    ReleaseDC(hwnd,hdc);
+    getchar();
     return 0;
 }
