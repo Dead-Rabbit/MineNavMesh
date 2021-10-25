4752578f5719655fd83aa1ca4dbea9b1e7a46b30
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2021-10-25 23:23:54 +0800

    git 新增easyx的引用

--- A_TriangulationByEarClipping/Main.cpp
+++ A_TriangulationByEarClipping/Main.cpp
@@ -1,7 +1,17 @@
+#include <graphics.h>
 #include <iostream>
 
-int main(int argc, char* argv[])
+// int main(int argc, char* argv[])
+// {
+//     std::cout << "Hello Triangulation By Ear Clipping!" << std::endl;
+//     return 0;
+// }
+
+#include <conio.h>         //为了使用_getch()
+int main()
 {
-    std::cout << "Hello Triangulation By Ear Clipping!" << std::endl;
-    return 0;
+    initgraph(640, 480);   // 创建绘图窗口，大小为 640x480 像素
+    circle(200, 200, 100); // 画圆，圆心(200, 200)，半径 100
+    _getch();              // 按任意键继续,回收任意一个字符
+    closegraph();          // 关闭绘图窗口
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MineNavMesh
 个人关于 寻路 的学习
+
+---
+> 在使用之前，请安装 easyx -> https://easyx.cn/download
