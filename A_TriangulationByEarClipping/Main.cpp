e437e519c780e68bd9fceac25480944d7598bb47
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2021-10-25 23:16:18 +0800

    modify main.cpp

--- A_TriangulationByEarClipping/Main.cpp
+++ A_TriangulationByEarClipping/Main.cpp
@@ -1,26 +1,7 @@
+#include <iostream>
 
-#pragma comment(lib,"user32")
-#pragma comment(lib,"gdi32")
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-HWND WINAPI GetConsoleWindow();
-int main(int argc,char *argv[])
+int main(int argc, char* argv[])
 {
-    HWND hwnd;
-    HDC hdc;
-    HPEN hpen;
-    hwnd = GetConsoleWindow();
-    hdc = GetDC(hwnd);
-    system("color F0");
-    system("cls");
-    TextOut(hdc,50,100,nullptr,12);
-    hpen=CreatePen(PS_SOLID,1,RGB(255,0,0));
-    SelectObject(hdc,hpen);
-    MoveToEx(hdc,20,20,NULL);
-    LineTo(hdc,200,300);
-    DeleteObject(hpen);
-    ReleaseDC(hwnd,hdc);
-    getchar();
+    std::cout << "Hello Triangulation By Ear Clipping!" << std::endl;
     return 0;
 }
