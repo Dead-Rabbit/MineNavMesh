7f762b2e7a55aac6bc773843467cce75b8e671bb
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2021-10-26 18:36:59 +0800

    创建测试用Vatti分割+耳切法三角形化的代码

--- C_Triangulation_After_Vatti/Main.cpp
+++ C_Triangulation_After_Vatti/Main.cpp
@@ -1,4 +1,5 @@
 #include "Sources/clipper.h"
+#include "Sources/clipper_core.hpp"
 #include "../Base/Graphes.h"
 
 #define USE_EASYX_GTAPHICS  // 是否使用 EasyX 进行输出，目前EasyX仅支持Windows平台
@@ -23,20 +24,20 @@ void DrawPath(int pathNum, Path<double> path, COLORREF color);
 
 #endif
 
-std::vector<Path<double>> DoClipTest();
+std::vector<Path<double>> DoClipTest(ClipperD* clipperD);
 std::vector<Path<double>> outputSubjectPaths;
 std::vector<Path<double>> outputClipPaths;
 
-ClipperD clipperD = ClipperD();
-
 int main(int argc, char* argv[])
 {
+    ClipperD* clipperD = new ClipperD();
+
     Path<double> subjectPath1 = Path<double>();
     subjectPath1.push_back(Point<double>(100, 100));
     subjectPath1.push_back(Point<double>(100, 300));
     subjectPath1.push_back(Point<double>(300, 300));
     subjectPath1.push_back(Point<double>(300, 100));
-    clipperD.AddPath(subjectPath1, PathType::Subject, false);
+    clipperD->AddPath(subjectPath1, PathType::Subject, false);
     outputSubjectPaths.push_back(subjectPath1);
     
     Path<double> subjectPath2 = Path<double>();
@@ -44,7 +45,7 @@ int main(int argc, char* argv[])
     subjectPath2.push_back(Point<double>(310, 300));
     subjectPath2.push_back(Point<double>(600, 200));
     subjectPath2.push_back(Point<double>(600, 100));
-    clipperD.AddPath(subjectPath2, PathType::Subject, false);
+    clipperD->AddPath(subjectPath2, PathType::Subject, false);
     outputSubjectPaths.push_back(subjectPath2);
     
     Path<double> clipPath1 = Path<double>();
@@ -52,7 +53,7 @@ int main(int argc, char* argv[])
     clipPath1.push_back(Point<double>(281, 324));
     clipPath1.push_back(Point<double>(324, 317));
     clipPath1.push_back(Point<double>(292, 120));
-    clipperD.AddPath(clipPath1, PathType::Clip, false);
+    clipperD->AddPath(clipPath1, PathType::Clip, false);
     outputClipPaths.push_back(clipPath1);
     
     Path<double> clipPath2 = Path<double>();
@@ -61,7 +62,7 @@ int main(int argc, char* argv[])
     clipPath2.push_back(Point<double>(421, 208));
     clipPath2.push_back(Point<double>(458, 161));
     clipPath2.push_back(Point<double>(436, 142));
-    clipperD.AddPath(clipPath2, PathType::Clip, false);
+    clipperD->AddPath(clipPath2, PathType::Clip, false);
     outputClipPaths.push_back(clipPath2);
     
     Path<double> clipPath3 = Path<double>();
@@ -70,7 +71,7 @@ int main(int argc, char* argv[])
     clipPath3.push_back(Point<double>(508, 197));
     clipPath3.push_back(Point<double>(528, 158));
     clipPath3.push_back(Point<double>(516, 134));
-    clipperD.AddPath(clipPath3, PathType::Clip, false);
+    clipperD->AddPath(clipPath3, PathType::Clip, false);
     outputClipPaths.push_back(clipPath3);
     
     Path<double> clipPath4 = Path<double>();
@@ -79,7 +80,7 @@ int main(int argc, char* argv[])
     clipPath4.push_back(Point<double>(190, 241));
     clipPath4.push_back(Point<double>(212, 200));
     clipPath4.push_back(Point<double>(194, 173));
-    clipperD.AddPath(clipPath4, PathType::Clip, false);
+    clipperD->AddPath(clipPath4, PathType::Clip, false);
     outputClipPaths.push_back(clipPath4);
     
     Path<double> clipPath5 = Path<double>();
@@ -88,7 +89,7 @@ int main(int argc, char* argv[])
     clipPath5.push_back(Point<double>(223, 342));
     clipPath5.push_back(Point<double>(310, 348));
     clipPath5.push_back(Point<double>(365, 309));
-    clipperD.AddPath(clipPath5, PathType::Clip, false);
+    clipperD->AddPath(clipPath5, PathType::Clip, false);
     outputClipPaths.push_back(clipPath5);
     
 #ifdef USE_EASYX_GTAPHICS
@@ -124,7 +125,7 @@ int main(int argc, char* argv[])
         case WM_LBUTTONDOWN:
         {
             ClearDrawBoard();
-            std::vector<Path<double>> resultPaths = DoClipTest();
+            std::vector<Path<double>> resultPaths = DoClipTest(clipperD);
             for (int i = 0; i < resultPaths.size(); i++)
             {
                 auto path = resultPaths[i];
@@ -147,11 +148,11 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-std::vector<Path<double>> DoClipTest()
+std::vector<Path<double>> DoClipTest(ClipperD* clipperD)
 {
     PathsD resultPaths = PathsD();
     // 获取最后输出path
-    if (clipperD.Execute(ClipType::Difference, FillRule::Negative, resultPaths))
+    if (clipperD->Execute(ClipType::Difference, FillRule::Negative, resultPaths))
     {
         return resultPaths.data;
     }
