96d2d24ca16fb3780f0057784acbf37730671ed9
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2021-11-30 15:15:49 +0800

    试验后，发现，用的这个KDTree怎么表现这么糟糕？？

--- G_KDTree/Main.cpp
+++ G_KDTree/Main.cpp
@@ -1,15 +1,31 @@
+#include <chrono>
+
 #include "../Base/Vectors.h"
 #include <graphics.h>
 #include <conio.h>      //为了使用_getch()
 #include <vector>
+
+#include <ctime>
+#include <time.h>
+
+#include "KDTree.h"
 
 NavMeshBase::Vector2* graphSize = new NavMeshBase::Vector2(640, 480);
 
 std::vector<NavMeshBase::Vector2> points;
 NavMeshBase::Vector2 mainPoint;
 
+int findNeighborNum = 1;
+
+float minDis = 100;
+
 int GEN_MIN_X = 20, GEN_MAX_X = 600;
 int GEN_MIN_Y = 20, GEN_MAX_Y = 440;
+
+int methodType = 0; // 0 为普通，1为KDTree
+std::chrono::microseconds duration;
+
+KDTree tree;
 
 void ClearBoard();
 void ReDrawBoard();
@@ -39,10 +55,11 @@ int main(int argc, char* argv[])
     setbkmode(TRANSPARENT);     // 去掉文字背景颜色
     settextstyle(20, 0, L"微软雅黑");
     
+    BeginBatchDraw();
     // 绘制当前调试用
     ClearBoard();
     ReDrawBoard();
-    
+
     ExMessage m;		// Define a message variable
     while(true)
     {
@@ -52,17 +69,150 @@ int main(int argc, char* argv[])
         {
         case WM_LBUTTONDOWN:
             {
-                // Test Length Compare To Find Neighborhood
-    
-                // Test KDTrees Find Neighborhood
-                
+                ClearBoard();
                 ReDrawBoard();
+
+                if (methodType == 0) methodType = 1; else methodType = 0;
+
+                if (methodType == 1)
+                {
+                    // auto start = std::chrono::steady_clock::now();
+
+                    // 构建树
+                    pointVec point_vec = pointVec();
+                    for (auto point : points)
+                    {
+                        point_t point_ts = point_t();
+                        point_ts.push_back(point.x);
+                        point_ts.push_back(point.y);
+                        point_vec.push_back(point_ts);
+                    }
+                    tree = KDTree(point_vec);
+                    // point_t main_point = point_t();
+                    // main_point.push_back(mainPoint.x);
+                    // main_point.push_back(mainPoint.y);
+                    // auto findNeighborPoints = tree.neighborhood_points(main_point, minDis);
+                    
+                    // auto finish = std::chrono::steady_clock::now();
+                    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+                    // std::cout << "Find By KDTree " << duration.count() << " ms" << std::endl;
+                    //
+                }
+                
+                // 测试遍历法
+                // {
+                //     auto start = std::chrono::steady_clock::now();
+                //
+                //     std::vector<NavMeshBase::Vector2> neighbors;
+                //     float squ_dis = minDis * minDis;
+                //     // 使用距离简单对比选择多个最近的
+                //     for (auto point : points)
+                //     {
+                //         auto dis = (point - mainPoint).squaredLength();
+                //         if (dis < squ_dis)
+                //         {
+                //             neighbors.push_back(point);
+                //         }
+                //     }
+                //     
+                //     auto finish = std::chrono::steady_clock::now();
+                //     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+                //     std::cout << "Find By Distance " << duration.count() << " ms" << std::endl;
+                //     
+                //     setfillcolor(GREEN);
+                //     for (const auto point : neighbors)
+                //     {
+                //         fillcircle(point.x, point.y, 3);
+                //     }
+                // }
+
+            }
+            break;
+        case WM_RBUTTONDOWN:
+            {
+                // // 测试 KDTree
+                // {
+                //     auto start = std::chrono::steady_clock::now();
+                //
+                //     // 构建树
+                //     pointVec point_vec = pointVec();
+                //     for (auto point : points)
+                //     {
+                //         point_t point_ts = point_t();
+                //         point_ts.push_back(point.x);
+                //         point_ts.push_back(point.y);
+                //         point_vec.push_back(point_ts);
+                //     }
+                //     tree = KDTree(point_vec);
+                //     point_t main_point = point_t();
+                //     main_point.push_back(mainPoint.x);
+                //     main_point.push_back(mainPoint.y);
+                //     auto findNeighborPoints = tree.neighborhood_points(main_point, minDis);
+                //     
+                //     auto finish = std::chrono::steady_clock::now();
+                //     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+                //     std::cout << "Find By KDTree " << duration.count() << " ms" << std::endl;
+                //     
+                //     setfillcolor(YELLOW);
+                //     for (auto point : findNeighborPoints)
+                //     {
+                //         fillcircle(point[0], point[1], 3);
+                //     }
+                // }
+            }
+            break;
+        case WM_MOUSEMOVE:
+            {
+                ClearBoard();
+                ReDrawBoard();
+                mainPoint = NavMeshBase::Vector2(m.x, m.y);
+                
+                auto start = std::chrono::steady_clock::now();
+
+                if (methodType == 0)
+                {
+                    std::vector<NavMeshBase::Vector2> neighbors;
+                    float squ_dis = minDis * minDis;
+                    // 使用距离简单对比选择多个最近的
+                    for (auto point : points)
+                    {
+                        auto dis = (point - mainPoint).squaredLength();
+                        if (dis < squ_dis)
+                        {
+                            neighbors.push_back(point);
+                        }
+                    }
+                    
+                    setfillcolor(GREEN);
+                    for (const auto point : neighbors)
+                    {
+                        fillcircle(point.x, point.y, 3);
+                    }
+                } else
+                {
+                    point_t main_point = point_t();
+                    main_point.push_back(mainPoint.x);
+                    main_point.push_back(mainPoint.y);
+                    auto findNeighborPoints = tree.neighborhood_points(main_point, minDis);
+                    
+                    setfillcolor(YELLOW);
+                    for (auto point : findNeighborPoints)
+                    {
+                        fillcircle(point[0], point[1], 3);
+                    }
+                }
+                
+                auto finish = std::chrono::steady_clock::now();
+                duration = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+                std::cout << duration.count() << " ms" << std::endl;
             }
             break;
         default:
             break;
         }
+        FlushBatchDraw();
     }
+    EndBatchDraw();
 }
 
 void ClearBoard()
@@ -74,12 +224,18 @@ void ClearBoard()
 
 void ReDrawBoard()
 {
+    settextstyle(25, 0, L"微软雅黑");
+    setcolor(BLACK);
+    TCHAR str[25];
+    _stprintf_s(str, _T("Current Method: %d", methodType));
+    outtextxy(0, 0, str);
+    
     setfillcolor(BLACK);
-    for (auto point : points)
+    for (const auto point : points)
     {
         fillcircle(point.x, point.y, 2);
     }
     
-    setfillcolor(RED);
-    fillcircle(mainPoint.x, mainPoint.y, 2);
+    setfillcolor(BLUE);
+    fillcircle(mainPoint.x, mainPoint.y, 4);
 }
